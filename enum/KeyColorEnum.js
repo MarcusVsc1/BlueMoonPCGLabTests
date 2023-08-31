055e024804a3a95835f35bae5718f3475e8275c6
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2023-08-31 07:34:43 -0300

    Renomeado KeyColorEnum e movido p/ arquivo externo

---
+++ enum/KeyColorEnum.js
@@ -0,0 +1,6 @@
+const KeyColorEnum = {
+    0: 'Amarela',
+    1: 'Roxa',
+    2: 'Verde',
+    3: 'Vermelha'
+}
--- puzzleagents/KeyAndDoorAgent.js
+++
@@ -1,6 +0,0 @@
-const KeyColor = {
-    0: 'Amarela',
-    1: 'Roxa',
-    2: 'Verde',
-    3: 'Vermelha'
-}
