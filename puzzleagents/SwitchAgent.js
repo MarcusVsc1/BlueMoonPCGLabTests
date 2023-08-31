b99df88e91b05ccd915192ef7c8f59c94f0fadf2
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2023-08-30 22:23:08 -0300

    Adicionada lógica do puzzle de switch

--- puzzleagents/SwitchAgent.js
+++ puzzleagents/SwitchAgent.js
@@ -1,11 +1,47 @@
 class SwitchAgent {
 
+    static id = 0;
+    static switchGroups = []
+
+    switchEvent() {
+        if(!this.toggled){
+            cena1.assets.play('switchOn2')
+            this.toggled = !this.toggled
+            var groupId = Math.floor((this.switchId-1)/4)
+            SwitchAgent.switchGroups[groupId].actualCombination.push(this.switchId)
+            var idx = SwitchAgent.switchGroups[groupId].actualCombination.indexOf(this.switchId)
+            if(SwitchAgent.switchGroups[groupId].correctCombination[idx] == SwitchAgent.switchGroups[groupId].actualCombination[idx]){
+                SwitchAgent.switchGroups[groupId].rewardCount++
+                if(SwitchAgent.switchGroups[groupId].rewardCount == 4) {
+                    cena1.dialogo = 'Conseguiu apertar na ordem certa.'
+                }
+            } else {
+                SwitchAgent.switchGroups[groupId].rewardCount = 0;
+                SwitchAgent.switchGroups[groupId].actualCombination = []
+                const numeroInicial = Math.floor((this.switchId-1) / 4) * 4;
+                for(var sprite of cena1.spritesO) {
+                    if(sprite.switchId-1 >= numeroInicial && sprite.switchId-1 < numeroInicial + 4){
+                        sprite.toggled = false
+                    }
+                }
+            }
+        }
+    }
+
     gerarPuzzle(mapGraph, puzzleGraph) {
         var room = mapGraph.nodes[0]
         var posicoes = this.posicoesDiagonaisElementoCentral(room)
+        var switches = []
         for(posicao of posicoes){
-            cena1.adicionar(gerenciador.criarInterruptor(posicao.x, posicao.y, null, 0)) 
+            cena1.adicionar(gerenciador.criarInterruptor(posicao.x, posicao.y, this.switchEvent,  ++SwitchAgent.id)) 
+            switches.push(SwitchAgent.id)
         }
+        SwitchAgent.switchGroups.push({
+            correctCombination: switches,
+            actualCombination:[],
+            rewardCount: 0,
+            reward: null
+        })
         
     }
 
